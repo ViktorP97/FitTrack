c6d3f36a4691ce500655969416f3cbd7473db84c
Viktor Prusak <viktor.prusak@gmail.com>
2024-04-15 20:37:47 +0200

    feat(): new controller for user sign, login and verification

---
+++ FitTrack/src/main/java/com/vp/fittrack/controllers/UserDataController.java
@@ -0,0 +1,109 @@
+package com.vp.fittrack.controllers;
+
+import com.vp.fittrack.dtos.LoginDto;
+import com.vp.fittrack.dtos.RegisterDto;
+import com.vp.fittrack.models.UserData;
+import com.vp.fittrack.services.UserDataService;
+import io.micrometer.common.util.StringUtils;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserDataController {
+
+  private final UserDataService userService;
+
+  @Autowired
+  public UserDataController(UserDataService userService) {
+    this.userService = userService;
+  }
+
+  @GetMapping("/register")
+  public String showRegistrationForm() {
+    return "registerUser";
+  }
+
+  @PostMapping("/register")
+  public String registerUser(RegisterDto registerDto, Model model) {
+    if (StringUtils.isBlank(registerDto.getName())) {
+      model.addAttribute("wrongInput", true);
+      model.addAttribute("missingInput", true);
+      return "registerUser";
+    }
+    if (StringUtils.isBlank(registerDto.getPassword())) {
+      model.addAttribute("wrongInput", true);
+      model.addAttribute("missingInput", true);
+      return "registerUser";
+    }
+    if (userService.nameExist(registerDto.getName())) {
+      model.addAttribute("wrongInput", true);
+      model.addAttribute("nameExist", true);
+      return "registerUser";
+    }
+    if (userService.emailExist(registerDto.getEmail())) {
+      model.addAttribute("wrongInput", true);
+      model.addAttribute("mailExist", true);
+      return "registerUser";
+    }
+    UserData user = userService.register(registerDto);
+    System.out.println("User registered: " + registerDto.getName());
+    return "redirect:/register/success/" + user.getToken();
+  }
+
+  @GetMapping("/verify/{mail}")
+  public String verifyUser(@PathVariable String mail) {
+    userService.verifyUser(mail);
+    return "verifyUser";
+  }
+
+  @GetMapping("/register/success/{token}")
+  public String verifyMessage(@PathVariable String token, Model model) {
+    UserData user = userService.findUserByToken(token);
+    if (user != null) {
+      model.addAttribute("mail", user.getEmail());
+      System.out.println("/verify/" + user.getEmail());
+    }
+    return "verificationMessage";
+  }
+
+  @GetMapping("/login")
+  public String loginUser() {
+    return "loginUser";
+  }
+
+  @PostMapping("/login")
+  public String mainPage(@ModelAttribute LoginDto loginDto, Model model) {
+    if (!userService.userRegistered(loginDto.getName())) {
+      model.addAttribute("wrongInput", true);
+      model.addAttribute("wrongName", loginDto.getName());
+      return "loginUser";
+    }
+    if (userService.wrongPassword(loginDto.getPassword(), loginDto.getName())) {
+      model.addAttribute("wrongInput", true);
+      model.addAttribute("wrongPassword", true);
+      return "loginUser";
+    }
+
+    if (userService.notVerified(loginDto.getName())) {
+      model.addAttribute("wrongInput", true);
+      model.addAttribute("notVerified", true);
+      return "loginUser";
+    }
+
+    UserData user = userService.logInUser(loginDto.getName());
+    return "redirect:/workout/zone/" + user.getId();
+  }
+
+  @GetMapping("/logout")
+  public String logout(HttpSession session) {
+    // Invalidate session and remove login attribute
+//    session.invalidate();
+    return "redirect:/login"; // Redirect to login page after logout
+  }
+}
