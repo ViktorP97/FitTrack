cd454a3b1e97aded97d778d0959ed9aa7a9e9c49
Viktor Prusak <viktor.prusak@gmail.com>
2024-04-22 19:39:30 +0200

    fix(): editing style

--- FitTrack/src/main/java/com/vp/fittrack/Exceptions/TrainingNameExistException.java
+++ FitTrack/src/main/java/com/vp/fittrack/Exceptions/TrainingNameExistException.java
@@ -1,6 +1,6 @@
 package com.vp.fittrack.Exceptions;
 
-public class TrainingNameExistException  extends RuntimeException {
+public class TrainingNameExistException extends RuntimeException {
 
   public TrainingNameExistException(String message) {
     super(message);
