aed74ae1755dc6cd31d7267b02b2eee6050bc0e9
Viktor Prusak <viktor.prusak@gmail.com>
2024-05-06 20:34:21 +0200

    feat(): new exception class when training is not found

---
+++ FitTrack/src/main/java/com/vp/fittrack/exceptions/TrainingNotFoundException.java
@@ -0,0 +1,8 @@
+package com.vp.fittrack.exceptions;
+
+public class TrainingNotFoundException extends RuntimeException {
+
+  public TrainingNotFoundException(String message) {
+    super(message);
+  }
+}
